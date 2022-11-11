f1b8166215bdc94220c06bad94359e145cae692b
Islom <pahlavonpulatov335@gmail.com>
2022-11-11 19:19:18 +0100

    degub chnaged to false

--- flightprice/settings.py
+++ flightprice/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rgr_gh^-=rby)682-d&4@2o8hpmbx_-$ow_(2_ra#$@x6542g@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
