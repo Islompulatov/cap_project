3ef0c4b2962e4b3454342f21da7f15cfe3327126
Islom <pahlavonpulatov335@gmail.com>
2022-11-11 20:12:02 +0100

    csrf secure

--- flightprice/settings.py
+++ flightprice/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = "django-insecure-rgr_gh^-=rby)682-d&4@2o8hpmbx_-$ow_(2_ra#$@x6542g@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-9fdc.up.railway.app/#predict']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ["*"]
 
